2ea64ef181b596a9acfe1d1c7102d02449ec9d70
Philipp Saake <philipp.saake@gmx.de>
2022-11-08 09:45:25 +0100

    part 1 done

--- Material_Part1_PEP/example3.py
+++ Material_Part1_PEP/example3.py
@@ -1,18 +1,23 @@
 def validate_data_dict(data_dict):
+    """ put explanation here
+    """
     if not data_dict:
         raise ValueError("data_dict is empty")
-    for something, otherthing in data_dict.items():
-        if not otherthing:
-            raise ValueError(f"The dict content under {something} is empty.")
-        if not isinstance(otherthing, dict):
+        
+    for item_one, item_two in data_dict.items():
+    
+        if not item_two:
+            raise ValueError(f"The dict content under {item_one} is empty.")
+            
+        if not isinstance(item_two, dict):
             raise ValueError(
-                f"The content of {something} is not a dict but {type(otherthing)}."
+                f"The content of {item_one} is not a dict but {type(item_two)}."
             )
 
-        list = ["data", "file_type", "sofa", "paragraph"]
+        list_ = ["data", "file_type", "sofa", "paragraph"]
         missing_cats = []
-        for category in list:
-            if category not in list(otherthing.keys()):
+        for category in list_:
+            if category not in list(item_two.keys()):
                 missing_cats.append(category)
 
         if missing_cats:
